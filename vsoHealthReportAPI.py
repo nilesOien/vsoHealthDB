40bf89785a4c04de87addf6374d492155e58cd41
Niles Oien <noien@nso.edu>
2026-05-26 14:42:53 -0600

    ruff check strikes

--- vsoHealthReportAPI.py
+++ vsoHealthReportAPI.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import List
 from dotenv import load_dotenv
